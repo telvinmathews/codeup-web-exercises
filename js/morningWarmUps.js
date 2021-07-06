60bf01e7951046d1e626417b53db69c9f1c540d9
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-07-06 09:33:14 -0500

    worked on morning exercie 2

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -1,22 +1,43 @@
 
-function countSpaces(str) {
-    var spaces = 0;
+// function countSpaces(str) {
+//     var spaces = 0;
+//
+//     if (typeof str !== 'string') {
+//         return false;
+//     }
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === " ") {
+//             spaces++;
+//         }
+//     }
+//     return spaces;
+// }
+// var result = countSpaces(' hakvbh njl  jalbxd');
+// var result2 = countSpaces(' bkha bjd');
+// var result3 = countSpaces('');
+// var result4 = countSpaces(' hjhb bjb jbj ');
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
 
-    if (typeof str !== 'string') {
-        return false;
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+function capitalizeAllNames(arrayOfStrings) {
+    for (let i = 0; i < arrayOfStrings; i++) {
+        arrayOfStrings[i].toUpperCase();
+        console.log(arrayOfStrings);
     }
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === " ") {
-            spaces++;
-        }
-    }
-    return spaces;
 }
-var result = countSpaces(' hakvbh njl  jalbxd');
-var result2 = countSpaces(' bkha bjd');
-var result3 = countSpaces('');
-var result4 = countSpaces(' hjhb bjb jbj ');
-console.log(result);
-console.log(result2);
-console.log(result3);
-console.log(result4);
+capitalizeAllNames(['bill','bob', 'jack']);
+
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+function capitalizeFirstLetter(arrayOfStrings) {
+    arrayOfStrings.forEach(string)
+
+}
